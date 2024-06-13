6b67c2565b7806e3868de49cdff0690c8b9939f0
Ngoc Khanh <Khanhk56a12@gmail.com>
2024-06-13 17:22:56 +0700

    - config 2 db for nautilus - add pyke module

--- thresh/src/main/java/vn/com/lol/thresh/modules/blob/service/impl/BlobServiceImpl.java
+++ thresh/src/main/java/vn/com/lol/thresh/modules/blob/service/impl/BlobServiceImpl.java
@@ -1,13 +1,26 @@
 package vn.com.lol.thresh.modules.blob.service.impl;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
+import vn.com.lol.thresh.commons.utils.StringUtil;
 import vn.com.lol.thresh.modules.blob.dtos.request.RegisterBlobRequest;
 import vn.com.lol.thresh.modules.blob.dtos.response.BlobDTO;
 import vn.com.lol.thresh.modules.blob.service.BlobService;
 
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import java.security.NoSuchAlgorithmException;
+
 @Service
-public class BlobServiceImpl implements BlobService {
-    public BlobDTO registerBlob(RegisterBlobRequest request) {
+public class BlobServiceImpl implements BlobService, CommandLineRunner {
+    public BlobDTO registerBlob(RegisterBlobRequest request) throws NoSuchPaddingException, NoSuchAlgorithmException {
+        Cipher cipher = Cipher.getInstance("RSA");
         return null;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        String ramdomString = StringUtil.generateRandomString(32);
+        System.out.println(ramdomString);
     }
 }
